7325ca02fd644bf77c030cf122ff897fdf714fbc
pafeuu <pawel.mejerr@gmail.com>
2025-07-23 09:08:47 +0200

    cherry grove pebbles and twigs

--- kubejs/server_scripts/tags/BiomeTags.js
+++ kubejs/server_scripts/tags/BiomeTags.js
@@ -4,4 +4,7 @@ ServerEvents.tags('worldgen/biome', event=>{
    event.add('twigs:spawns_pebble',/biomeswevegone*/)
    event.add('twigs:spawns_twig','#forge:is_overworld')
    event.add('twigs:spawns_twig',/biomeswevegone*/)
+
+   event.add('twigs:spawns_pebble',"minecraft:cherry_grove")
+   event.add('twigs:spawns_twig',"minecraft:cherry_grove")
 })
