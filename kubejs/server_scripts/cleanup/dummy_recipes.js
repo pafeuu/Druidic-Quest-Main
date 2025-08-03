c0a47dc2b1b448c136c3ae1cb751c2422ecb08e0
pafeuu <pawel.mejerr@gmail.com>
2025-08-03 16:06:15 +0200

    dummy recipes cleanup

--- kubejs/server_scripts/cleanup/dummy_recipes.js
+++ kubejs/server_scripts/cleanup/dummy_recipes.js
@@ -53,6 +53,25 @@ ServerEvents.recipes(event=>{
     event.smelting("wizards_reborn:raw_arcane_gold","wizards_reborn:arcane_gold_ore").id("wizards_reborn:smelting/arcane_gold_ingot_from_raw")
     event.blasting("wizards_reborn:raw_arcane_gold","wizards_reborn:arcane_gold_ore").id("wizards_reborn:blasting/arcane_gold_ingot_from_raw")
 
-    event.recipes.ars_nouveau.enchanting_apparatus("ars_nouveau:dominion_wand","kubejs:recipe_changed","kubejs:recipe_changed").id("ars_nouveau:dominion_wand")
+    event.recipes.ars_nouveau.enchanting_apparatus("kubejs:recipe_changed","kubejs:recipe_changed","kubejs:recipe_changed").id("ars_nouveau:dominion_wand")
+    
+    event.recipes.naturesaura.tree_ritual("kubejs:recipe_changed",["kubejs:recipe_changed"]).id("naturesaura:tree_ritual/conversion_catalyst")
+    event.recipes.naturesaura.tree_ritual("kubejs:recipe_changed",["kubejs:recipe_changed"]).id("naturesaura:tree_ritual/crushing_catalyst")
+    event.recipes.naturesaura.tree_ritual("kubejs:recipe_changed",["kubejs:recipe_changed"]).id("naturesaura:tree_ritual/furnace_heater")
+    event.shaped('kubejs:recipe_changed',["R"],{R:"kubejs:recipe_changed"}).id('ars_nouveau:basic_spell_turret')
+    event.shaped('kubejs:recipe_changed', ['kubejs:recipe_changed']).id("ars_nouveau:imbuement_chamber")
+
+    event.recipes.ars_nouveau.imbuement("kubejs:recipe_changed","kubejs:recipe_changed",100,[]).id("ars_nouveau:imbuement_air_essence")
+	event.recipes.ars_nouveau.imbuement("kubejs:recipe_changed","kubejs:recipe_changed",100,[]).id("ars_nouveau:imbuement_earth_essence")
+	event.recipes.ars_nouveau.imbuement("kubejs:recipe_changed","kubejs:recipe_changed",100,[]).id("ars_nouveau:imbuement_fire_essence")
+	event.recipes.ars_nouveau.imbuement("kubejs:recipe_changed","kubejs:recipe_changed",100,[]).id("ars_nouveau:imbuement_water_essence")
+
+    event.shaped('kubejs:recipe_changed',["R"],{R:"kubejs:recipe_changed"}).id('ars_nouveau:rotating_spell_turret')
+    event.recipes.ars_nouveau.enchanting_apparatus("kubejs:recipe_changed","kubejs:recipe_changed",'kubejs:recipe_changed').id('ars_nouveau:spell_turret')
+    event.recipes.ars_nouveau.enchanting_apparatus("kubejs:recipe_changed","kubejs:recipe_changed",'kubejs:recipe_changed').id('ars_nouveau:timer_spell_turret')
+    event.recipes.ars_nouveau.enchanting_apparatus(['kubejs:recipe_changed'],'kubejs:recipe_changed',"kubejs:recipe_changed").id("ars_nouveau:starbuncle_charm")
+    event.recipes.ars_nouveau.enchanting_apparatus(['kubejs:recipe_changed'],'kubejs:recipe_changed',"kubejs:recipe_changed").id("ars_nouveau:whirlisprig_charm")
+    event.recipes.ars_nouveau.enchanting_apparatus(['kubejs:recipe_changed'],'kubejs:recipe_changed',"kubejs:recipe_changed").id("ars_nouveau:drygmy_charm")
+    event.recipes.ars_nouveau.enchanting_apparatus(['kubejs:recipe_changed'],'kubejs:recipe_changed',"kubejs:recipe_changed").id("ars_nouveau:wixie_charm")
     
 })
