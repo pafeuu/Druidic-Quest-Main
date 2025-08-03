0b5afd9e5e2da55e8403cc7bb4c4487cb06c151e
pafeuu <pawel.mejerr@gmail.com>
2025-08-03 18:18:36 +0200

    Magic Saplings and seeds

--- kubejs/server_scripts/multiblocks/tree_ritual.js
+++ kubejs/server_scripts/multiblocks/tree_ritual.js
@@ -80,5 +80,116 @@ ServerEvents.recipes(event=>{
 
      //============================================= Seeds and Saplings =============================================
 
-     //tree_ritual()
+     tree_ritual("wizards_reborn:arcane_linen_seeds",
+          [
+               "wheat_seeds",
+               "wheat_seeds",
+               "#forge:dusts/gold",
+               "#forge:dusts/gold",
+               "wizards_reborn:arcanum_dust",
+               "wizards_reborn:arcanum_dust",
+               "wizards_reborn:arcanum_dust",
+               "wizards_reborn:arcanum_dust"
+          ],
+          "acacia_sapling"
+     ).id("wizards_reborn:wissen_crystallizer/arcane_linen_seeds")
+
+     tree_ritual("ars_nouveau:magebloom_crop",
+          [
+               "wizards_reborn:arcane_linen_seeds",
+               "wizards_reborn:arcane_linen_seeds",
+               "ars_nouveau:sourceberry_bush",
+               "ars_nouveau:sourceberry_bush",
+               "ars_nouveau:source_gem",
+               "ars_nouveau:source_gem",
+               "ars_nouveau:source_gem",
+               "ars_nouveau:source_gem"
+          ],
+          "ars_nouveau:purple_archwood_sapling"
+     )
+
+     tree_ritual("wizards_reborn:cork_bamboo_seed",
+          [
+               "wizards_reborn:arcane_linen_seeds",
+               "wizards_reborn:arcane_linen_seeds",
+               "bamboo",
+               "bamboo",
+               "bamboo",
+               "bamboo",
+               "bamboo",
+               "bamboo"
+          ],
+          "spruce_sapling"
+     )
+
+     tree_ritual("2x wizards_reborn:arcanum_seed",
+          [
+               "wizards_reborn:arcane_linen_seeds",
+               "wizards_reborn:arcane_linen_seeds",
+               "ars_nouveau:magebloom_crop",
+               "ars_nouveau:magebloom_crop",
+               "wizards_reborn:arcanum_dust",
+               "wizards_reborn:arcanum_dust",
+               "wizards_reborn:arcanum_dust",
+               "wizards_reborn:arcanum_dust"
+          ],
+          "wizards_reborn:arcane_wood_sapling"
+     ).id("wizards_reborn:crystal_infusion/arcanum_seed")
+
+     tree_ritual("chorus_flower",
+          [
+               "wizards_reborn:cork_bamboo",
+               "wizards_reborn:cork_bamboo",
+               "ender_pearl",
+               "ender_pearl",
+               "ars_nouveau:source_gem",
+               "ars_nouveau:source_gem",
+               "ars_nouveau:source_gem",
+               "ars_nouveau:source_gem"
+          ],
+          "biomeswevegone:indigo_jacaranda_sapling"
+     )
+
+     tree_ritual("forbidden_arcanus:golden_orchid_seeds",
+          [
+               "wizards_reborn:arcanum_seed",
+               "#forge:storage_blocks/deorum",
+               "#forge:storage_blocks/deorum",
+               "#forge:storage_blocks/deorum",
+               "#forge:storage_blocks/deorum",
+               "#forge:storage_blocks/deorum",
+               "#forge:storage_blocks/deorum",
+               "#forge:storage_blocks/deorum",
+          ],
+          "aether:golden_oak_sapling"
+     ).id("forbidden_arcanus:golden_orchid_seeds")
+
+     tree_ritual("wizards_reborn:arcane_wood_sapling",
+          [
+               "naturesaura:ancient_sapling",
+               "#forge:dusts/gold",
+               "wizards_reborn:arcanum_dust",
+               "wizards_reborn:arcanum_dust",
+               "wizards_reborn:arcanum_dust",
+               "wizards_reborn:arcanum_dust",
+               "wizards_reborn:arcanum_dust",
+               "wizards_reborn:arcanum_dust",
+          ],
+          "biomeswevegone:blue_spruce_sapling"
+     )
+
+     tree_ritual("wizards_reborn:innocent_wood_sapling",
+          [
+               "wizards_reborn:arcane_wood_sapling",
+               "diamond",
+               "diamond",
+               "diamond",
+               "quartz_block",
+               "quartz_block",
+               "quartz_block",
+               "quartz_block"
+          ],
+          "cherry_sapling"
+     ).id("wizards_reborn:crystal_infusion/innocent_wood_sapling")
+     
 })
