b661a366f486faa2e76872fcaca4fab3fe790898
pafeuu <pawel.mejerr@gmail.com>
2025-08-03 16:07:02 +0200

    tree ritual cleanup

--- kubejs/server_scripts/multiblocks/tree_ritual.js
+++ kubejs/server_scripts/multiblocks/tree_ritual.js
@@ -1,5 +1,6 @@
 ServerEvents.recipes(event=>{
 
+     const tree_ritual = event.recipes.naturesaura.tree_ritual
     /*Ingredients order
             1
          5     7
@@ -7,7 +8,7 @@ ServerEvents.recipes(event=>{
          6     8
             2
     */
-    event.recipes.naturesaura.tree_ritual("wizards_reborn:arcane_workbench",
+     tree_ritual("wizards_reborn:arcane_workbench",
         ["crafting_table",
         "wizards_reborn:arcane_wood_planks",
         "naturesaura:gold_leaf",
@@ -18,10 +19,8 @@ ServerEvents.recipes(event=>{
         "wizards_reborn:arcane_wood_planks"],
         "birch_sapling").id("wizards_reborn:shaped/arcane_workbench")
    
-   
-   event.recipes.ars_nouveau.enchanting_apparatus(['kubejs:recipe_changed'],'kubejs:recipe_changed',"ars_nouveau:starbuncle_charm").id("ars_nouveau:starbuncle_charm")
 
-   event.recipes.naturesaura.tree_ritual("ars_nouveau:starbuncle_charm",
+     tree_ritual("ars_nouveau:starbuncle_charm",
         ["ars_nouveau:starbuncle_shards",
         "woodenhopper:wooden_hopper",
         "immersiveengineering:conveyor_basic",
@@ -33,9 +32,7 @@ ServerEvents.recipes(event=>{
         "ars_nouveau:green_archwood_sapling")
    
    
-   event.recipes.ars_nouveau.enchanting_apparatus(['kubejs:recipe_changed'],'kubejs:recipe_changed',"ars_nouveau:whirlisprig_charm").id("ars_nouveau:whirlisprig_charm")
-   
-   event.recipes.naturesaura.tree_ritual("ars_nouveau:whirlisprig_charm",
+     tree_ritual("ars_nouveau:whirlisprig_charm",
         ["ars_nouveau:whirlisprig_shards",
         "enigmaticlegacy:infinimeal",
         "kubejs:life_essence",
@@ -46,9 +43,7 @@ ServerEvents.recipes(event=>{
         "kubejs:nature_essence"],
         "ars_nouveau:green_archwood_sapling")
 
-   event.recipes.ars_nouveau.enchanting_apparatus(['kubejs:recipe_changed'],'kubejs:recipe_changed',"ars_nouveau:drygmy_charm").id("ars_nouveau:drygmy_charm")
-
-   event.recipes.naturesaura.tree_ritual("ars_nouveau:drygmy_charm",
+     tree_ritual("ars_nouveau:drygmy_charm",
         ["ars_nouveau:drygmy_shard",
         "golden_sword",
         "golden_sword",
@@ -58,9 +53,8 @@ ServerEvents.recipes(event=>{
         "kubejs:death_essence",
         "kubejs:death_essence"],
         "ars_nouveau:green_archwood_sapling")
-     
-   event.recipes.ars_nouveau.enchanting_apparatus(['kubejs:recipe_changed'],'kubejs:recipe_changed',"ars_nouveau:wixie_charm").id("ars_nouveau:wixie_charm")
-   event.recipes.naturesaura.tree_ritual("ars_nouveau:wixie_charm",
+
+     tree_ritual("ars_nouveau:wixie_charm",
         ["ars_nouveau:wixie_shards",
         "naturesaura:auto_crafter",
         "woodenhopper:wooden_hopper",
@@ -71,8 +65,8 @@ ServerEvents.recipes(event=>{
         "minecraft:amethyst_shard"],
         "ars_nouveau:purple_archwood_sapling")
 
-     event.remove({output:"ars_nouveau:enchanting_apparatus"})
-     event.recipes.naturesaura.tree_ritual("ars_nouveau:enchanting_apparatus",
+     
+     tree_ritual("ars_nouveau:enchanting_apparatus",
           ["quark:diamond_heart",
           "#forge:plates/diamond",
           "#forge:gears/gold",
@@ -83,4 +77,8 @@ ServerEvents.recipes(event=>{
           "#forge:ingots/source_alloy"],
           "ars_nouveau:blue_archwood_sapling"
      )
+
+     //============================================= Seeds and Saplings =============================================
+
+     //tree_ritual()
 })
