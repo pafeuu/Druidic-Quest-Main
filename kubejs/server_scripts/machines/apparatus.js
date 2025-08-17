36a5e8c96191c62b8efbee4c99f5f12713f29eaf
pafeuu <pawel.mejerr@gmail.com>
2025-08-17 19:21:34 +0200

    Enchanting Apparatus recipes

--- kubejs/server_scripts/machines/apparatus.js
+++ kubejs/server_scripts/machines/apparatus.js
@@ -8,5 +8,45 @@ ServerEvents.recipes(event=>{
         "ars_nouveau:arcane_core"],
         "kubejs:magic_machine",
         "wizards_reborn:arcane_iterator").id("wizards_reborn:arcane_workbench/arcane_iterator")
-   
+
+    apparatus([
+        "ars_nouveau:fire_essence",
+        "ars_nouveau:fire_essence",
+        "ars_nouveau:air_essence",
+        "ars_nouveau:air_essence",
+        "ars_nouveau:water_essence",
+        "ars_nouveau:water_essence",
+        "ars_nouveau:earth_essence",
+        "ars_nouveau:earth_essence"
+    ],"kubejs:infused_diamond","kubejs:elemental_core",5000)
+
+    apparatus([
+        "ars_nouveau:fire_essence",
+        "ars_nouveau:fire_essence",
+        "ars_nouveau:air_essence",
+        "ars_nouveau:air_essence",
+        "ars_nouveau:water_essence",
+        "ars_nouveau:water_essence",
+        "ars_nouveau:earth_essence",
+        "ars_nouveau:earth_essence"
+    ],"quark:diamond_heart","kubejs:elemental_core",2000)
+
+    event.remove({output:"immersiveengineering:alloybrick"})
+
+    apparatus([
+        "ars_nouveau:air_essence",
+        "ars_nouveau:water_essence",
+        "ars_nouveau:fire_essence",
+        "ars_nouveau:earth_essence",
+        "#forge:plates/tainted_gold"
+    ],"kubejs:magic_machine","immersiveengineering:alloybrick",5000)
+
+    apparatus([
+        "naturesaura:token_joy",
+        "kubejs:nature_essence",
+        "kubejs:nature_essence",
+        "kubejs:nature_essence",
+        "kubejs:nature_essence"
+    ],"kubejs:basic_magic_machine","kubejs:infusion_catalyst",5000)
+
 })
