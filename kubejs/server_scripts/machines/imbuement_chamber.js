1e1e165e88f680d6f9441de1eac5782b2fc49131
pafeuu <pawel.mejerr@gmail.com>
2025-08-18 08:11:07 +0200

    altarnate recipes in imbuement chamber

--- kubejs/server_scripts/machines/imbuement_chamber.js
+++ kubejs/server_scripts/machines/imbuement_chamber.js
@@ -1,5 +1,10 @@
 ServerEvents.recipes(e=>{
     
-    
+    const imbuement = e.recipes.ars_nouveau.imbuement;
+
+    const SmallSourceAmount = 500;
+    imbuement("#forge:foods/meat/raw","minecraft:rotten_flesh",SmallSourceAmount,["rotten_flesh","endrem:undead_soul","rotten_flesh"])
+    imbuement("#forge:foods/meat/raw","minecraft:bone",SmallSourceAmount,["bone","endrem:undead_soul","bone"])
+    imbuement("#forge:stripped_logs","naturesaura:wood_stand",SmallSourceAmount,["#forge:tools/knives","naturesaura:gold_leaf","naturesaura:gold_leaf"])
     
 })
