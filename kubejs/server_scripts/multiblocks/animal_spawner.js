95d44992f25c6a911cb35696a441a64551dab68f
pafeuu <pawel.mejerr@gmail.com>
2025-08-31 11:16:48 +0200

    Alex's Animal spawner part 1

--- kubejs/server_scripts/multiblocks/animal_spawner.js
+++ kubejs/server_scripts/multiblocks/animal_spawner.js
@@ -1,85 +1,258 @@
 ServerEvents.recipes((event)=>{
 
-    event.recipes.naturesaura.animal_spawner("ars_nouveau:whirlisprig",
+    const MagicalCritterAuraCost = 80000;
+    const RegularAnimalAuraCost = 50000
+    const spawn = event.recipes.naturesaura.animal_spawner;
+    spawn("ars_nouveau:whirlisprig",
         ["naturesaura:birth_spirit",
         "#minecraft:saplings",
-        "kubejs:nature_essence"],50000,60)
+        "kubejs:nature_essence"],MagicalCritterAuraCost,60)
 
-    event.recipes.naturesaura.animal_spawner("ars_nouveau:drygmy",
+    spawn("ars_nouveau:drygmy",
         ["naturesaura:birth_spirit",
         "ars_nouveau:wilden_horn",
-        "kubejs:nature_essence"],50000,60)
+        "kubejs:nature_essence"],MagicalCritterAuraCost,60)
 
-    event.recipes.naturesaura.animal_spawner("ars_nouveau:starbuncle",
+    spawn("ars_nouveau:starbuncle",
         ["naturesaura:birth_spirit",
         "#forge:nuggets/gold",
-        "kubejs:nature_essence"],50000,60)
+        "kubejs:nature_essence"],MagicalCritterAuraCost,60)
 
-    event.recipes.naturesaura.animal_spawner("allay",
+    spawn("allay",
         ["naturesaura:birth_spirit",
         "amethyst_shard",
-        "kubejs:nature_essence"],50000,60)
+        "kubejs:nature_essence"],MagicalCritterAuraCost,60)
 
-    event.recipes.naturesaura.animal_spawner("irons_spellbooks:pyromancer",
+    spawn("irons_spellbooks:pyromancer",
         ["naturesaura:birth_spirit",
         "kubejs:infused_emerald",
         "irons_spellbooks:legendary_ink",
         "irons_spellbooks:fire_rune"],200000,100)
     
-    event.recipes.naturesaura.animal_spawner("irons_spellbooks:cryomancer",
+    spawn("irons_spellbooks:cryomancer",
         ["naturesaura:birth_spirit",
         "kubejs:infused_emerald",
         "irons_spellbooks:legendary_ink",
         "irons_spellbooks:ice_rune"],200000,100)
     
-    event.recipes.naturesaura.animal_spawner("irons_spellbooks:priest",
+    spawn("irons_spellbooks:priest",
         ["naturesaura:birth_spirit",
         "kubejs:infused_emerald",
         "irons_spellbooks:legendary_ink",
         "irons_spellbooks:holy_rune"],200000,100)
     
-    event.recipes.naturesaura.animal_spawner("irons_spellbooks:cultist",
+    spawn("irons_spellbooks:cultist",
         ["naturesaura:birth_spirit",
         "kubejs:infused_emerald",
         "irons_spellbooks:legendary_ink",
         "irons_spellbooks:blood_rune"],200000,100)
 
-    event.recipes.naturesaura.animal_spawner("irons_spellbooks:archevoker",
+    spawn("irons_spellbooks:archevoker",
         ["naturesaura:birth_spirit",
         "kubejs:infused_emerald",
         "irons_spellbooks:legendary_ink",
         "irons_spellbooks:evocation_rune"],200000,100)
 
-    event.recipes.naturesaura.animal_spawner("irons_spellbooks:apothecarist",
+    spawn("irons_spellbooks:apothecarist",
         ["naturesaura:birth_spirit",
         "kubejs:infused_emerald",
         "irons_spellbooks:legendary_ink",
         "irons_spellbooks:nature_rune"],200000,100)
 
-    event.recipes.naturesaura.animal_spawner("irons_spellbooks:necromancer",
+    spawn("irons_spellbooks:necromancer",
         ["naturesaura:birth_spirit",
         "irons_spellbooks:arcane_essence",
         "irons_spellbooks:uncommon_ink",
-        "bone"],50000,60)
+        "bone"],MagicalCritterAuraCost,60)
 
-    event.recipes.naturesaura.animal_spawner("irons_spellbooks:ancient_knight",
+    spawn("irons_spellbooks:ancient_knight",
         ["naturesaura:birth_spirit",
         "irons_spellbooks:arcane_essence",
         "irons_spellbooks:epic_ink",
         "netherite_scrap"],100000,80)
 
-    event.recipes.naturesaura.animal_spawner("ars_nouveau:wilden_guardian",
+    spawn("ars_nouveau:wilden_guardian",
         ["naturesaura:birth_spirit",
         "ars_nouveau:wilden_spike",
-        "ars_nouveau:source_gem"],50000,60)
+        "ars_nouveau:source_gem"],MagicalCritterAuraCost,60)
     
-    event.recipes.naturesaura.animal_spawner("ars_nouveau:wilden_hunter",
+    spawn("ars_nouveau:wilden_hunter",
         ["naturesaura:birth_spirit",
         "ars_nouveau:wilden_horn",
-        "ars_nouveau:source_gem"],50000,60)
+        "ars_nouveau:source_gem"],MagicalCritterAuraCost,60)
 
-    event.recipes.naturesaura.animal_spawner("ars_nouveau:wilden_stalker",
+    spawn("ars_nouveau:wilden_stalker",
         ["naturesaura:birth_spirit",
         "ars_nouveau:wilden_wing",
-        "ars_nouveau:source_gem"],50000,60)
+        "ars_nouveau:source_gem"],MagicalCritterAuraCost,60)
+
+    spawn("alexsmobs:bunfungus",
+        ["naturesaura:birth_spirit",
+        "rabbit",
+        "#c:mushrooms"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:bison",
+        ["naturesaura:birth_spirit",
+        "beef",
+        "brown_carpet"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:flutter",
+        ["naturesaura:birth_spirit",
+        "flowering_azalea"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:rocky_roller",
+        ["naturesaura:birth_spirit",
+        "pointed_dripstone",
+        "tuff"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:seagull",
+        ["naturesaura:birth_spirit",
+        "feather",
+        "#minecraft:fishes"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:bald_eagle",
+        ["naturesaura:birth_spirit",
+        "feather",
+        "leather"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:tiger",
+        ["naturesaura:birth_spirit",
+        "orange_wool",
+        "leather"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:mungus",
+        ["naturesaura:birth_spirit",
+        "#c:mushrooms",
+        "farmersdelight:iron_knife"],RegularAnimalAuraCost,60)
+    
+    spawn("alexsmobs:guster",
+        ["naturesaura:birth_spirit",
+        "quark:bottled_cloud",
+        "sand"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:kangaroo",
+        ["naturesaura:birth_spirit",
+        "bundle",
+        "leather"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:sugar_glider",
+        ["naturesaura:birth_spirit",
+        "sugar",
+        "leather"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:anaconda",
+        ["naturesaura:birth_spirit",
+        "supplementaries:rope",
+        "twilightforest:naga_scale"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:underminer",
+        ["naturesaura:birth_spirit",
+        "quark:soul_bead",
+        "iron_pickaxe"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:hammerhead_shark",
+        ["naturesaura:birth_spirit",
+        "immersiveengineering:hammer",
+        "#minecraft:fishes"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:toucan",
+        ["naturesaura:birth_spirit",
+        "feather",
+        "yellow_dye"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:blue_jay",
+        ["naturesaura:birth_spirit",
+        "feather",
+        "blue_dye"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:potoo",
+        ["naturesaura:birth_spirit",
+        "feather",
+        "spider_eye"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:crow",
+        ["naturesaura:birth_spirit",
+        "feather",
+        "black_dye"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:elephant",
+        ["naturesaura:birth_spirit",
+        "supplementaries:faucet"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:tusklin",
+        ["naturesaura:birth_spirit",
+        "porkchop",
+        "ars_nouveau:wilden_spike"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:gelada_monkey",
+        ["naturesaura:birth_spirit",
+        "grass",
+        "alexsmobs:banana"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:banana_slug",
+        ["naturesaura:birth_spirit",
+        "slime_ball",
+        "alexsmobs:banana"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:mudskipper",
+        ["naturesaura:birth_spirit",
+        "mud"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:lobster",
+        ["naturesaura:birth_spirit",
+        "#minecraft:fishes",
+        "red_dye",
+        "#forge:plates/stone"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:seal",
+        ["naturesaura:birth_spirit",
+        "gray_wool",
+        "#minecraft:fishes",
+        "thermal:copper_coin"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:rhinoceros",
+        ["naturesaura:birth_spirit",
+        "ars_nouveau:wilden_horn",
+        "#forge:plates/stone"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:alligator_snapping_turtle",
+        ["naturesaura:birth_spirit",
+        "minecraft:scute",
+        "ars_nouveau:wilden_spike"],RegularAnimalAuraCost,60)
+    
+    spawn("alexsmobs:flying_fish",
+        ["naturesaura:birth_spirit",
+        "#minecraft:fishes",
+        "ars_nouveau:wilden_wing"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:mimic_octopus",
+        ["naturesaura:birth_spirit",
+        "silly_oddities:rainbow_bundle",
+        "forbidden_arcanus:tentacle"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:rain_frog",
+        ["naturesaura:birth_spirit",
+        "frogspawn",
+        "sand"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:capuchin_monkey",
+        ["naturesaura:birth_spirit",
+        "alexsmobs:banana",
+        "alexsmobs:maggot"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:capuchin_monkey",
+        ["naturesaura:birth_spirit",
+        "alexsmobs:banana",
+        "alexsmobs:maggot"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:gorilla",
+        ["naturesaura:birth_spirit",
+        "alexsmobs:banana",
+        "#minecraft:leaves"],RegularAnimalAuraCost,60)
+
+    spawn("alexsmobs:platypus",
+        ["naturesaura:birth_spirit",
+        "clay",
+        "red_dye"],RegularAnimalAuraCost,60)
+    
 })
