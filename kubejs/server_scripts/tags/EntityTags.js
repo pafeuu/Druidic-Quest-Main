b806cdc716265397f15816266128a4395d1d0af3
pafeuu <pawel.mejerr@gmail.com>
2025-09-11 09:07:41 +0200

    Bosses blacklist

---
+++ kubejs/server_scripts/tags/EntityTags.js
@@ -0,0 +1,46 @@
+ServerEvents.tags("entity_type",e=>{
+
+    e.add("forge:bosses",[
+        "minecraft:ender_dragon",
+        "minecraft:wither",
+        "twilightforest:hydra",
+        "twilightforest:naga",
+        "twilightforest:minoshroom",
+        "twilightforest:lich",
+        "twilightforest:knight_phantom",
+        "twilightforest:ur_ghast",
+        "twilightforest:alpha_yeti",
+        "twilightforest:quest_ram",
+        "twilightforest:snow_queen",
+        "aether:slider",
+        "aether:sun_spirit",
+        "aether:valkyrie_queen",
+        "ancient_aether:mutated_aechor_plant",
+        "deep_aether:eots_controller",
+        "deep_aether:eots_segment",
+        "alexsmobs:void_worm"
+    ])
+
+    e.add("forge:mini_bosses",[
+    "minecraft:evoker",
+    "irons_spellbooks:archevoker",
+    "irons_spellbooks:cultist",
+    "irons_spellbooks:cryomancer",
+    "irons_spellbooks:pyromancer",
+    "irons_spellbooks:priest",
+    "irons_spellbooks:apothecarist",
+    "minecraft:elder_guardian",
+    "minecraft:warden",
+    "alexsmobs:warped_mosco"
+  ])
+
+    e.add("ars_nouveau:drygmy_blacklist","#forge:bosses")
+    e.add("ars_nouveau:drygmy_blacklist","#forge:mini_bosses")
+
+    e.add("ars_nouveau:jar_blacklist","#forge:bosses")
+    e.add("ars_nouveau:jar_blacklist","#forge:mini_bosses")
+
+    e.add("forbidden_arcanus:quantum_catcher_blacklisted","#forge:bosses")
+    e.add("forbidden_arcanus:quantum_catcher_blacklisted","#forge:mini_bosses")
+
+})
