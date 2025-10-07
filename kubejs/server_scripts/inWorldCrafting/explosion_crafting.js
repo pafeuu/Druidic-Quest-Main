62be20ef01838eda778b88b913cd157d4cb204a2
pafeuu <pawel.mejerr@gmail.com>
2025-10-07 09:29:51 +0200

    lychee cleanup

---
+++ kubejs/server_scripts/inWorldCrafting/explosion_crafting.js
@@ -0,0 +1,166 @@
+ServerEvents.recipes(event=>{
+
+
+    const ParticleFire = {type: "execute",command: "particle irons_spellbooks:fire ~ ~ ~ 0 0 0 0.02 15",hide: true}
+
+    const ParticleGreenFire = {type: "execute",command: "particle supplementaries:green_flame ~ ~ ~ 0 0 0 0.05 80",hide: true}
+
+    const ParticleGold = {type: "execute",command: 'particle irons_spellbooks:wisp ~ ~ ~ 0.3 0.3 0.3 0.1 50',"hide": true}
+
+    const ParticleLightning = {type: "execute",command: 'particle irons_spellbooks:electricity ~ ~ ~ 0 0 0 0.05 30',"hide": true}
+
+    const ParticleBubbles = {type: "execute",command: "particle deep_aether:poison_bubbles ~ ~ ~ 0 0 0 0.1 20",hide: true}
+
+    const ParticleEnd = {type: "execute", command: "particle irons_spellbooks:unstable_ender ~ ~ ~ 0.5 0.5 0.5 0.1 80 force",hide: true}
+
+    const SoundEffectLightning = {type: "execute",command: "playsound irons_spellbooks:lightning_lance_cast neutral @p",hide: true}
+
+    const SoundEffectDimension = {type: "execute",command: "playsound minecraft:block.portal.travel neutral @p",hide: true} 
+
+    const SoundEffectTransmutation = {type: "execute",command: "playsound wizards_reborn:arcanum_dust_transmutation neutral @p",hide: true}
+
+    const SoundEffectEarthquake = {type: "execute",command: "playsound irons_spellbooks:spell.earthquake.cast neutral @p",hide: true}
+
+    const ConditionDimensionNether = {type: "location",predicate: {dimension: "minecraft:the_nether"}}
+
+    const ConditionUnderground = {type: "location",predicate: {position:{ y: {min: -64,max: 20}}}}
+
+    const ConditionSky = {type: "location",predicate: {position:{y: {min: 190, max: 320}}}}
+
+
+    function ExplosionCrafting(amount, output, ingredients, particle, sound, condition) {
+
+        const itemInputs = ingredients.map(i => (i.tag ? { tag: i.tag } : { item: i.item }));
+
+        
+        const recipe = {
+            type: "lychee:item_exploding",
+            item_in: itemInputs,
+            post: [
+                { type: "delay", s: 2 },
+                { type: "drop_item", item: output, count: amount },
+                sound,
+                particle
+            ]
+        };
+
+        // Only add contextual if a condition was provided
+        if (condition) {
+            recipe.contextual = Array.isArray(condition) ? contextual : [condition];
+        }
+
+        event.custom(recipe);
+    }
+
+    ExplosionCrafting(12,"ars_nouveau:fire_essence",[
+        { item: "kubejs:nature_essence" },
+        { item: "kubejs:nature_essence" },
+        { item: "kubejs:nature_essence" },
+        { item: "kubejs:nature_essence" },
+        { item: "kubejs:fire_infused_arcanum" },
+        { item: "kubejs:fire_infused_arcanum" },
+        { item: "kubejs:fire_infused_arcanum" },
+        { item: "kubejs:fire_infused_arcanum" },
+        { item: "ars_nouveau:source_gem" },
+        { item: "ars_nouveau:source_gem" },
+        { item: "ars_nouveau:source_gem" },
+        { item: "ars_nouveau:source_gem" },
+        { item: 'kubejs:sunbird_feather' }
+    ],ParticleFire,
+    SoundEffectTransmutation)
+
+    ExplosionCrafting(1,"ars_nouveau:fire_essence",[
+        { item: "kubejs:nature_essence" },
+        { item: "kubejs:fire_infused_arcanum" },
+        { item: "ars_nouveau:source_gem" },
+        { item: 'blaze_rod' }
+    ],ParticleFire,
+    SoundEffectTransmutation,
+    ConditionDimensionNether)
+
+
+    ExplosionCrafting(1,"ars_nouveau:earth_essence",[
+        {item: "kubejs:nature_essence"},
+		{item: "kubejs:earth_infused_arcanum"},
+		{item: "ars_nouveau:source_gem"},
+		{item: "minecraft:bone"}
+    ],ParticleGreenFire,
+    SoundEffectEarthquake,
+    ConditionUnderground)
+
+    event.remove({output:"naturesaura:animal_spawner"})
+    ExplosionCrafting(1,"naturesaura:animal_spawner",[
+        {tag: "forge:storage_blocks/gold"},
+		{item: "minecraft:hay_block"},
+		{item: "minecraft:moss_block"},
+		{item: "kubejs:broken_spawner"}
+    ],ParticleGreenFire,
+    SoundEffectEarthquake)
+
+    ExplosionCrafting(4,"naturesaura:gold_fiber",[
+        {item: "minecraft:string",},
+		{item: "minecraft:string",},
+		{item: "minecraft:wheat_seeds",},
+		{item: "minecraft:wheat_seeds",},
+		{tag: "forge:flowers/golden",},
+		{tag: "forge:flowers/golden",},
+		{tag: "forge:flowers/golden",},
+		{tag: "forge:flowers/golden",}
+    ],ParticleGold,
+    SoundEffectTransmutation)
+
+    ExplosionCrafting(1,"kubejs:dimension_ripper",[
+        {item: "quark:diamond_heart"},
+		{item: "forbidden_arcanus:ender_pearl_fragment"},
+		{item: "diamond"},
+		{item: "redstone_block"}
+    ],ParticleEnd,
+    SoundEffectDimension)
+
+    event.remove({id:"irons_spellbooks:lightning_bottle"})
+    ExplosionCrafting(1,"irons_spellbooks:lightning_bottle",[
+        {item: "quark:bottled_cloud"},
+		{item: "minecraft:lightning_rod"}
+    ],ParticleLightning,
+    SoundEffectLightning)
+	event.custom({
+		type: "lychee:item_exploding",
+		  "contextual": [
+			{
+			  type: "location",
+			  "predicate": {
+				"position":{ 
+				"y": {
+				 "min": 180,
+				 "max": 210
+				}
+				}
+			  }
+			}
+		  ],
+		  post: [
+			{
+			  type: "drop_item",
+			  item: "irons_spellbooks:lightning_bottle"
+			},
+			{
+				type: "execute",
+				command: "execute playsound minecraft:entity.lightning_bolt.impact neutral @p",
+				hide: true
+				
+			}
+		  ],
+		  item_in: [
+			{
+			  item: "quark:bottled_cloud"
+			},
+			{
+			  item: "minecraft:lightning_rod"
+			}
+		  ]
+	})
+
+
+
+    
+})
--- kubejs/server_scripts/multiblocks/multiblock_crafting.js
+++ kubejs/server_scripts/multiblocks/multiblock_crafting.js
@@ -123,122 +123,7 @@ ServerEvents.recipes(event => {
     })
   }
 
-  function ClickingSquareCrafting(catalyst, output, sides, corners, middle){
-
-    event.custom({
-      "type": "lychee:block_interacting",
-      "hide_in_viewer": true,
-      "comment": "Needs additional blocks below the item to work. Check the ponder for details",
-      "block_in": middle,
-      "item_in": {
-        "item": catalyst // Item that needs to be on top of the structure in the middle
-      },
-      "contextual": {
-        "type": "and",
-        "contextual": [ // Checks if the the blocks are in the correct position
-          {
-            "type": "execute",
-            "command": `execute if block ~1 ~ ~1 ${corners}`,
-            "hide": true
-          },
-          {
-            "type": "execute",
-            "command": `execute if block ~-1 ~ ~-1 ${corners}`,
-            "hide": true
-          },
-          {
-            "type": "execute",
-            "command": `execute if block ~1 ~ ~-1 ${corners}`,
-            "hide": true
-          },
-          {
-            "type": "execute",
-            "command": `execute if block ~-1 ~ ~1 ${corners}`,
-            "hide": true
-          },
-          {
-            "type": "execute",
-            "command": `execute if block ~ ~ ~1 ${sides}`,
-            "hide": true
-          },
-          {
-            "type": "execute",
-            "command": `execute if block ~ ~ ~-1 ${sides}`,
-            "hide": true
-          },
-          {
-            "type": "execute",
-            "command": `execute if block ~1 ~ ~ ${sides}`,
-            "hide": true
-          },
-          {
-            "type": "execute",
-            "command": `execute if block ~-1 ~ ~ ${sides}`,
-            "hide": true
-          }
-      
-        ],
-        "hide": true
-      },
-      "post": [
-        {
-          "type": "execute",
-          "command": `fill ~1 ~-1 ~1 ~-1 ~ ~-1 minecraft:air replace ${middle}`, // Clears blocks
-          "hide": true
-        },
-        {
-          "type": "execute",
-          "command": `fill ~1 ~-1 ~1 ~-1 ~ ~-1 minecraft:air replace ${sides}`, // Clears blocks
-          "hide": true
-        },
-        {
-          "type": "execute",
-          "command": `fill ~1 ~-1 ~1 ~-1 ~ ~-1 minecraft:air replace ${corners}`, // Clears blocks
-          "hide": true
-        },
-        {
-          "type": "execute",
-          "command": 'particle irons_spellbooks:unstable_ender ~ ~-1 ~ 1.5 0.5 1.5 0.1 240 force',
-          "hide": true
-        },
-        {
-          "type": "delay",
-          "s": 1
-        },
-        {
-          "type": "drop_item",
-          "item": output
-        },
-        {
-          "type": "execute",
-          "command": 'particle irons_spellbooks:wisp ~ ~ ~ 0.3 0.3 0.3 0.1 80 force',
-          "hide": true
-        },
-        {
-          "type": "execute",
-          "command": "playsound forbidden_arcanus:item.mundabitur_dust.use neutral @a",
-          "hide": true
-        }
-      ]
-    })
-
-    event.custom({
-      "type": "lychee:block_interacting",
-      "hide_in_viewer": false,
-      "ghost":true,
-      "comment": "Needs additional blocks below the item to work. Check the ponder for details",
-      "item_in": {
-        "item": catalyst // Item that needs to be on top of the structure in the middle
-      },
-      "post": [
-        {
-          "type": "drop_item",
-          "item": output
-        }
-      ]
-    })
-  }
-
+  
   function Infusing(result, amount, ingredients) {
     const contextualConditions = [
         { "type": "execute", "command": 'execute if block ~1 ~-1 ~ rubinated_nether:altar_stone_tiles', "hide": true },
@@ -354,9 +239,6 @@ ServerEvents.recipes(event => {
     { type: 'item', name: 'kubejs:alchemical_dust' }
   ]);
 
-  ClickingSquareCrafting("thermal:coal_coke","minecraft:furnace","quark:sturdy_stone","quark:sturdy_stone","minecraft:coal_block")
-  
-  ClickingSquareCrafting("kubejs:lemon_quartz","kubejs:magical_generator_block","kubejs:source_alloy_block","kubejs:arcanum_alloy_block","kubejs:source_alloy_block")
   
 
 })
