81a83e26b686ed10109727cd0e1a74e97316eb7e
pafeuu <pawel.mejerr@gmail.com>
2026-01-24 10:41:00 +0100

    valkyrum accessories

---
+++ kubejs/server_scripts/utility/invFrames.js
@@ -0,0 +1,25 @@
+// Based on the script by .mbninja
+
+var invultick = true // Var to reset invulnerable time
+
+EntityEvents.hurt("minecraft:player",event => {
+    const { entity } = event
+    const extraInvTicks = 0
+    if(!entity.isCuriosEquipped("ancient_aether:valkyrum_ring") && !entity.isCuriosEquipped("ancient_aether:valkyrum_pendant"))
+        return;
+    if(entity.isCuriosEquipped("ancient_aether:valkyrum_ring"))
+        extraInvTicks=+4
+
+    if(entity.isCuriosEquipped("ancient_aether:valkyrum_pendant"))
+        extraInvTicks=+6
+
+    
+    if ( invultick == true ) {
+        entity.invulnerableTime = 20+extraInvTicks //Gets Value from total armor and adds it to iframe timer
+        invultick = false
+    }
+
+    if ( entity.invulnerableTime <= 10 ) {
+    invultick = true //if your iframes are less then 10, you will take damage. This will let you get hit once.
+    }
+})
