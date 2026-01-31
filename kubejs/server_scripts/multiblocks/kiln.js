9ca5260b1239de5f0e9f17073616cad51527903b
pafeuu <pawel.mejerr@gmail.com>
2026-01-31 08:29:46 +0100

    more alloys p2

--- kubejs/server_scripts/multiblocks/kiln.js
+++ kubejs/server_scripts/multiblocks/kiln.js
@@ -145,6 +145,21 @@ ServerEvents.recipes(event=>{
 		"16x ars_nouveau:air_essence"
 	)
 
+	kilnWith2Ingredients("2x irons_spellbooks:arcane_ingot",
+		"2x kubejs:arcanum_alloy_ingot",
+		"8x irons_spellbooks:arcane_essence"
+	)
+	
+	kilnWith3Ingredients("thermal:electrum_ingot",
+		"4x kubejs:lightning_essence",
+		"immersiveengineering:ingot_aluminum",
+		"wizards_reborn:arcane_gold_ingot")
+
+	kilnWith3Ingredients("immersiveengineering:ingot_steel",
+		"thermal:steel_ingot",
+		"immersiveengineering:ingot_aluminum",
+		"8x kubejs:infused_amethyst")
+
 	arcFurnace("kubejs:arcane_plating","4x wizards_reborn:arcane_gold_sheet",["4x netherite_ingot","8x thermal:ruby"])
 	
 	arcFurnace("ancient_aether:valkyrum","naturesaura:sky_ingot",["16x kubejs:life_essence","64x ars_nouveau:air_essence","16x thermal:sapphire"])
@@ -152,5 +167,11 @@ ServerEvents.recipes(event=>{
 	arcFurnace("deep_aether:stratus_ingot","16x aether:enchanted_gravitite",["8x thermal:ruby","8x thermal:sapphire"])
 
 	arcFurnace("deep_aether:stratus_ingot","16x kubejs:infused_amethyst",["16x kubejs:infused_diamond","16x thermal:ruby","16x thermal:sapphire","16x kubejs:infused_emerald"])
+
+	arcFurnace("2x immersiveengineering:ingot_steel","thermal:steel_ingot",[
+		"immersiveengineering:ingot_aluminum",
+		"8x kubejs:infused_amethyst",
+		"thermal:nickel_ingot"
+	])
 
 })
