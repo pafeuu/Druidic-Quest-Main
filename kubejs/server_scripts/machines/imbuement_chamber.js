6b2eb4bace2e772de9f2575c899f9cc2edcebb79
pafeuu <pawel.mejerr@gmail.com>
2026-01-31 08:30:00 +0100

    automatable wood stand

--- kubejs/server_scripts/machines/imbuement_chamber.js
+++ kubejs/server_scripts/machines/imbuement_chamber.js
@@ -6,5 +6,6 @@ ServerEvents.recipes(e=>{
     imbuement("#forge:foods/meat/raw","minecraft:rotten_flesh",SmallSourceAmount,["rotten_flesh","kubejs:corrupted_soul","rotten_flesh"])
     imbuement("#forge:foods/meat/raw","minecraft:bone",SmallSourceAmount,["bone","kubejs:corrupted_soul","bone"])
     imbuement("#forge:stripped_logs","naturesaura:wood_stand",SmallSourceAmount,["#forge:tools/knives","naturesaura:gold_leaf","naturesaura:gold_leaf"])
+    imbuement("naturesaura:wood_stand","kubejs:basic_token",SmallSourceAmount,["#forge:tools/knives","naturesaura:token_joy","naturesaura:token_grief"])
     
 })
