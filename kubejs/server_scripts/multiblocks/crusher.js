a9d8bf2ffd5453f48cec9f3ff80288944ff2e72a
pafeuu <pawel.mejerr@gmail.com>
2026-04-09 08:28:28 +0200

    crusher recipes

---
+++ kubejs/server_scripts/multiblocks/crusher.js
@@ -0,0 +1,5 @@
+ServerEvents.recipes(event=>{
+
+    event.recipes.immersiveengineering.crusher("4x sand","sandstone").id("immersiveengineering:crusher/sandstone")
+    event.recipes.immersiveengineering.crusher("4x red_sand","red_sandstone").id("immersiveengineering:crusher/red_sandstone")
+})
