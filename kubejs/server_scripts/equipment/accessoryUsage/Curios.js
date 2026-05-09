5c063cd38c02845b681562ac4249d60612c831ef
pafeuu <pawel.mejerr@gmail.com>
2026-05-09 14:29:35 +0200

    doubled myclieal ring

--- kubejs/server_scripts/equipment/accessoryUsage/Curios.js
+++ kubejs/server_scripts/equipment/accessoryUsage/Curios.js
@@ -30,17 +30,3 @@ PlayerEvents.tick(event=>{
     }
     
 })
-
-PlayerEvents.tick(event=>{
-    
-    let player = event.player
-
-    if(player.age%200)
-        return
-
-    if(player.isCuriosEquipped("kubejs:mycelial_ring"))
-    {
-        player.addFood(1,0.3)
-    }
-
-})
