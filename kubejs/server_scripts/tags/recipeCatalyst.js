8c756bc3cf90d40f1d6494567c3e88cf603e95a5
pafeuu <pawel.mejerr@gmail.com>
2026-05-09 14:29:07 +0200

    lychee catalyst

--- kubejs/server_scripts/tags/recipeCatalyst.js
+++ kubejs/server_scripts/tags/recipeCatalyst.js
@@ -1,18 +1,24 @@
-ServerEvents.tags('item',event=>{
+ServerEvents.tags("item", (event) => {
+  const explodingCatalysts = [
+    "ars_nouveau:glyph_explosion",
+    "kubejs:explosive_arrow",
+    "thermal:explosive_grenade",
+    "minecraft:tnt_minecart",
+    "minecraft:creeper_spawn_egg",
+  ];
 
-    const explodingCatalysts = [
-        "ars_nouveau:glyph_explosion",
-        "kubejs:explosive_arrow",
-        "thermal:explosive_grenade",
-        "minecraft:tnt_minecart",
-        "minecraft:creeper_spawn_egg"
-    ]
+  const lightningCatalysts = [
+    "aether:lightning_knife",
+    "kubejs:lightning_arrow",
+    "thermal:lightning_charge",
+    "thermal:lightning_tnt",
+    "thermal:lightning_grenade",
+    "ars_nouveau:glyph_lightning",
+  ];
 
-    event.add("lychee:item_exploding_catalysts",explodingCatalysts)
+  event.add("lychee:item_exploding_catalysts", explodingCatalysts);
 
-    event.add("lychee:block_exploding_catalysts",explodingCatalysts)
+  event.add("lychee:block_exploding_catalysts", explodingCatalysts);
 
-    event.add("lychee:lightning_channeling_catalysts","aether:lightning_knife")
-
-    
-})
+  event.add("lychee:lightning_channeling_catalysts", lightningCatalysts);
+});
