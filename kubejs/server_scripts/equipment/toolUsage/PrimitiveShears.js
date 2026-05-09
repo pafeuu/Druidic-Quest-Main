dc1c1353fac7f60a4a7b657a126febff81b422c7
pafeuu <pawel.mejerr@gmail.com>
2026-05-09 14:27:21 +0200

    logs

--- kubejs/server_scripts/equipment/toolUsage/PrimitiveShears.js
+++ kubejs/server_scripts/equipment/toolUsage/PrimitiveShears.js
@@ -1,13 +1,9 @@
-ItemEvents.entityInteracted('kubejs:primitive_shears',event =>{
-
-    const entityName = event.getTarget().entityType
-    console.log("attack")
-    //let entityName = event.getTarget().entityType
-    //event.target.attack(2)
-    if (entityName == "entity.minecraft.sheep")
-    {
-        console.log("this is a sheep")
-        event.target.attack(2)
-
-    }
-})
+ItemEvents.entityInteracted("kubejs:primitive_shears", (event) => {
+  const entityName = event.getTarget().entityType;
+  console.log("attack");
+  //let entityName = event.getTarget().entityType
+  //event.target.attack(2)
+  if (entityName == "entity.minecraft.sheep") {
+    event.target.attack(2);
+  }
+});
