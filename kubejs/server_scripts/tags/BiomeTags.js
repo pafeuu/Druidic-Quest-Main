5d53c88ea502d029d690f29c3810ec62c5f61728
pafeuu <pawel.mejerr@gmail.com>
2026-05-09 14:29:43 +0200

    biome tags fix

--- kubejs/server_scripts/tags/BiomeTags.js
+++ kubejs/server_scripts/tags/BiomeTags.js
@@ -1,11 +1,6 @@
 ServerEvents.tags("worldgen/biome", (event) => {
-  event.add("twigs:spawns_pebble", "#forge:is_overworld");
-  event.add("twigs:spawns_pebble", /biomeswevegone*/);
-  event.add("twigs:spawns_twig", "#forge:is_overworld");
-  event.add("twigs:spawns_twig", /biomeswevegone*/);
-
-  event.add("twigs:spawns_pebble", "minecraft:cherry_grove");
-  event.add("twigs:spawns_twig", "minecraft:cherry_grove");
+  event.add("twigs:spawns_pebble", "#minecraft:is_overworld");
+  event.add("twigs:spawns_twig", "#minecraft:is_overworld");
 
   //event.add('dungeons_arise:has_structure/coliseum_biomes',)
 });
