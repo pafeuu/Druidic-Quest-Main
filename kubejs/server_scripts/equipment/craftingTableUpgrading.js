d1f2845f3ece773a12495d3be667004b902f46c1
pafeuu <pawel.mejerr@gmail.com>
2026-05-16 11:01:18 +0200

    arctic and yeti armor

--- kubejs/server_scripts/equipment/craftingTableUpgrading.js
+++ kubejs/server_scripts/equipment/craftingTableUpgrading.js
@@ -1,4 +1,4 @@
-ServerEvents.recipes(event=>{
+ServerEvents.recipes(event => {
 
     event.shaped("irons_spellbooks:infernal_sorcerer_chestplate",
         [
@@ -34,27 +34,42 @@ ServerEvents.recipes(event=>{
         return result.withNBT(input.nbt)
     }).id("irons_spellbooks:paladin_chestplate")
 
-    //This crashes the game. Probably because of colored leather armor.
-    
-    event
-    .shaped("twilightforest:naga_chestplate", ["SLS", "SSS", "SSS"], {
-      S: "twilightforest:naga_scale",
-      L: "leather_chestplate",
+    event.shaped("twilightforest:naga_chestplate", ["SLS", "SSS", "SSS"], {
+        S: "twilightforest:naga_scale",
+        L: "leather_chestplate",
     }).modifyResult((grid, result) => {
         let input = grid.find("minecraft:leather_chestplate");
         return result.withNBT(input.nbt)
-    })
-    .id("twilightforest:equipment/naga_chestplate");
+    }).id("twilightforest:equipment/naga_chestplate");
 
-  event
-    .shaped("twilightforest:naga_leggings", ["SSS", "SLS", "S S"], {
-      S: "twilightforest:naga_scale",
-      L: "leather_leggings",
+    event.shaped("twilightforest:naga_leggings", ["SSS", "SLS", "S S"], {
+        S: "twilightforest:naga_scale",
+        L: "leather_leggings",
     }).modifyResult((grid, result) => {
         let input = grid.find("minecraft:leather_leggings");
         return result.withNBT(input.nbt)
-    })
-    .id("twilightforest:equipment/naga_leggings");
+    }).id("twilightforest:equipment/naga_leggings");
+
+
+    global.armorSlots.forEach(slot => {
+
+        event.shaped(`twilightforest:yeti_${slot}`, [" S ", "SLS", " S "], {
+            S: "twilightforest:alpha_yeti_fur",
+            L: `twilightforest:arctic_${slot}`,
+        }).modifyResult((grid, result) => {
+            let input = grid.find(`twilightforest:arctic_${slot}`);
+            return result.withNBT(input.nbt)
+        }).id(`twilightforest:equipment/yeti_${slot}`);
+
+        event.shaped(`twilightforest:arctic_${slot}`, [" S ", "SLS", " S "], {
+            S: "twilightforest:arctic_fur",
+            L: `minecraft:leather_${slot}`,
+        }).modifyResult((grid, result) => {
+            let input = grid.find(`minecraft:leather_${slot}`);
+            return result.withNBT(input.nbt)
+        }).id(`twilightforest:equipment/arctic_${slot}`);
+
+    });
 
 
 })
