2ec66f6e17387cde13a6346b12af3771d342027b
pafeuu <pawel.mejerr@gmail.com>
2026-06-24 21:34:37 +0200

    pebbles with pickaxe

--- kubejs/server_scripts/inWorldCrafting/block_clicking.js
+++ kubejs/server_scripts/inWorldCrafting/block_clicking.js
@@ -1,6 +1,6 @@
-ServerEvents.recipes(event=>{
+ServerEvents.recipes(event => {
 
-    function ClickingSquareCrafting(catalyst, output, sides, corners, middle){
+  function ClickingSquareCrafting(catalyst, output, sides, corners, middle) {
 
     event.custom({
       "type": "lychee:block_interacting",
@@ -53,7 +53,7 @@ ServerEvents.recipes(event=>{
             "command": `execute if block ~-1 ~ ~ ${sides}`,
             "hide": true
           }
-      
+
         ],
         "hide": true
       },
@@ -91,14 +91,14 @@ ServerEvents.recipes(event=>{
           "command": 'particle irons_spellbooks:wisp ~ ~ ~ 0.3 0.3 0.3 0.1 80 force',
           "hide": true
         },
-        { type: "execute",command: "playsound wizards_reborn:arcanum_dust_transmutation neutral @p",hide: true}
+        { type: "execute", command: "playsound wizards_reborn:arcanum_dust_transmutation neutral @p", hide: true }
       ]
     })
 
     event.custom({
       "type": "lychee:block_interacting",
       "hide_in_viewer": false,
-      "ghost":true,
+      "ghost": true,
       "block_in": middle,
       "comment": "Needs additional blocks below the item to work. Check the ponder for details",
       "item_in": {
@@ -111,99 +111,132 @@ ServerEvents.recipes(event=>{
         }
       ]
     })
-    }
+  }
 
-    ClickingSquareCrafting("thermal:coal_coke","minecraft:furnace","quark:sturdy_stone","quark:sturdy_stone","minecraft:coal_block")
-  
-    ClickingSquareCrafting("kubejs:lemon_quartz","kubejs:magical_generator_block","kubejs:source_alloy_block","kubejs:arcanum_alloy_block","kubejs:source_alloy_block")
+  ClickingSquareCrafting("thermal:coal_coke", "minecraft:furnace", "quark:sturdy_stone", "quark:sturdy_stone", "minecraft:coal_block")
 
-    const clickB = event.recipes.create.item_application
+  ClickingSquareCrafting("kubejs:lemon_quartz", "kubejs:magical_generator_block", "kubejs:source_alloy_block", "kubejs:arcanum_alloy_block", "kubejs:source_alloy_block")
 
-    clickB("twilightforest:liveroot_block",["twilightforest:root","kubejs:nature_essence"])
-    clickB("crying_obsidian",["obsidian","kubejs:cut_onions"])
-    clickB("immersiveengineering:slag_glass",["glass","thermal:slag"])
-    clickB("ars_nouveau:source_jar",["vintagedelight:fermenting_jar","kubejs:source_alloy_ingot"])
-    clickB("create:rose_quartz_block",["minecraft:quartz_block","minecraft:rose_bush"])
-    
-    event.remove([
-      {id:"create:rose_quartz_block_from_rose_quartz_stonecutting"},
-      {id:"create:rose_quartz_tiles_from_polished_rose_quartz_stonecutting"},
-      {id:"create:small_rose_quartz_tiles_from_polished_rose_quartz_stonecutting"}])
+  const clickB = event.recipes.create.item_application
 
-    event.custom({
-		"type": "lychee:block_interacting",
-		"comment": "one of the items needs to be in the offhand",
-		"post": [
-		  {"type": "place", "block": "naturesaura:wood_stand"},
-		  {"type": "damage_item", "damage": 1, "target": "/item_in/0"},
-		  {"type": "execute","command": "playsound sawmill:ui.sawmill.take_result neutral @p","hide": true},
-		  {"type": "execute","command": "advancement grant @p only naturesaura:wood_stand","hide": true},
-		  {"type": "execute","command": "ftbquests change_progress @p complete 5A3EA25913807A1A","hide": true}
-		],
-		"item_in": [
-		  {"tag": "forge:tools/knives"},
-		  {"item": "naturesaura:gold_leaf"}
-		],
-		"block_in": {"tag": "forge:stripped_logs"}
-	  }).id("naturesaura:wood_stand")
+  clickB("twilightforest:liveroot_block", ["twilightforest:root", "kubejs:nature_essence"])
+  clickB("crying_obsidian", ["obsidian", "kubejs:cut_onions"])
+  clickB("immersiveengineering:slag_glass", ["glass", "thermal:slag"])
+  clickB("ars_nouveau:source_jar", ["vintagedelight:fermenting_jar", "kubejs:source_alloy_ingot"])
+  clickB("create:rose_quartz_block", ["minecraft:quartz_block", "minecraft:rose_bush"])
 
-    event.custom({
-	  type: "lychee:block_interacting",
-	  post: [
-		{
-		  type: "hurt",
-		  damage: 1
-		},
-		{
-			type: "drop_item",
-			"contextual": [
-				{
-				type: "chance",
-				"chance": 0.8
-				}
-			],
-			item: "twigs:pebble",
-			"count": 1
-		},
-		{
-			type: "add_item_cooldown",
-			"s": 0.5
-	    }
-	  ],
-	  item_in: {
-		item: "minecraft:air"
-	  },
-	  block_in: "minecraft:stone"
-	})
+  event.remove([
+    { id: "create:rose_quartz_block_from_rose_quartz_stonecutting" },
+    { id: "create:rose_quartz_tiles_from_polished_rose_quartz_stonecutting" },
+    { id: "create:small_rose_quartz_tiles_from_polished_rose_quartz_stonecutting" }])
 
   event.custom({
-		type: "lychee:block_interacting",
-		post: [
-			{ type: "drop_item", item: "kubejs:big_enchanting_rune", count: 1 },
-			{ type: "execute",command: "playsound wizards_reborn:arcanum_dust_transmutation neutral @p",hide: true},
-            { type: "place", block: "air"}
-		],
-		item_in: [
-			{ item: "ars_nouveau:manipulation_essence" }
-		],
-        block_in: "kubejs:deepslate_rune"
-	})
+    "type": "lychee:block_interacting",
+    "comment": "one of the items needs to be in the offhand",
+    "post": [
+      { "type": "place", "block": "naturesaura:wood_stand" },
+      { "type": "damage_item", "damage": 1, "target": "/item_in/0" },
+      { "type": "execute", "command": "playsound sawmill:ui.sawmill.take_result neutral @p", "hide": true },
+      { "type": "execute", "command": "advancement grant @p only naturesaura:wood_stand", "hide": true },
+      { "type": "execute", "command": "ftbquests change_progress @p complete 5A3EA25913807A1A", "hide": true }
+    ],
+    "item_in": [
+      { "tag": "forge:tools/knives" },
+      { "item": "naturesaura:gold_leaf" }
+    ],
+    "block_in": { "tag": "forge:stripped_logs" }
+  }).id("naturesaura:wood_stand")
 
-  
+  event.custom({
+    type: "lychee:block_interacting",
+    post: [
+      {
+        type: "hurt",
+        damage: 1
+      },
+      {
+        type: "drop_item",
+        "contextual": [
+          {
+            type: "chance",
+            "chance": 0.8
+          }
+        ],
+        item: "twigs:pebble",
+        "count": 1
+      },
+      {
+        type: "add_item_cooldown",
+        "s": 0.5
+      }
+    ],
+    item_in: {
+      item: "minecraft:air"
+    },
+    block_in: "minecraft:stone"
+  })
 
-    /*function TallFlowers(flower)
-    {
-      event.custom({
-		"type": "lychee:block_interacting",
-		"post": [
-		  {"type": "drop_item", "item": flower},
-		],
-		"item_in": [
-		  {"item": "bone_meal"}
-		],
-		"block_in": flower
-	  })
-    }
+  event.custom({
+    type: "lychee:block_interacting",
+    post: [
+      {
+        type: "damage_item",
+        damage: 1
+      },
+      {
+      "type": "place",
+      "contextual": [
+          {
+            type: "chance",
+            "chance": 0.1
+          }
+        ],
+      "block": "cobblestone"
+      },
+      {
+        type: "drop_item",
+        item: "twigs:pebble",
+        count: 1
+      },
+      {
+        type: "add_item_cooldown",
+        "s": 0.5
+      }
+    ],
+    item_in: {
+      tag: "minecraft:pickaxe"
+    },
+    block_in: "minecraft:stone"
+  })
 
-    TallFlowers("minecraft:pitcher_plant")*/
+  event.custom({
+    type: "lychee:block_interacting",
+    post: [
+      { type: "drop_item", item: "kubejs:big_enchanting_rune", count: 1 },
+      { type: "execute", command: "playsound wizards_reborn:arcanum_dust_transmutation neutral @p", hide: true },
+      { type: "place", block: "air" }
+    ],
+    item_in: [
+      { item: "ars_nouveau:manipulation_essence" }
+    ],
+    block_in: "kubejs:deepslate_rune"
+  })
+
+
+
+  /*function TallFlowers(flower)
+  {
+    event.custom({
+  "type": "lychee:block_interacting",
+  "post": [
+    {"type": "drop_item", "item": flower},
+  ],
+  "item_in": [
+    {"item": "bone_meal"}
+  ],
+  "block_in": flower
+  })
+  }
+
+  TallFlowers("minecraft:pitcher_plant")*/
 })
