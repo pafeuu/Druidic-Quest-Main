c99712e5e4adf21062e4aa5f51bbd219f85c28a0
pafeuu <pawel.mejerr@gmail.com>
2026-07-06 10:20:55 +0200

    more lanterns on the chains

---
+++ kubejs/server_scripts/tags/connectibleChains.js
@@ -0,0 +1,33 @@
+ServerEvents.tags("item", event => {
+
+    event.add("connectiblechains:hangable_items", [
+        'rubinated_nether:ruby_lantern',
+        'rubinated_nether:ruby_chandelier',
+        'ancient_aether:wynd_lantern',
+        'ancient_aether:sun_lantern',
+        'quark:paper_lantern_sakura',
+        'ancient_aether:ambrosium_lantern',
+        'deep_aether:skyjade_lantern',
+        'twigs:paper_lantern',
+        'twigs:allium_paper_lantern',
+        'twigs:blue_orchid_paper_lantern',
+        'twigs:crimson_roots_paper_lantern',
+        'twigs:dandelion_paper_lantern',
+        'twigs:torchflower_paper_lantern',
+        /*'wizards_reborn:arcane_salt_lantern',
+        'wizards_reborn:innocent_salt_lantern',
+        'wizards_reborn:cork_bamboo_salt_lantern',
+        'wizards_reborn:wisestone_salt_lantern',
+        'silly_oddities:oak_salt_lantern',
+        'silly_oddities:spruce_salt_lantern',
+        'silly_oddities:birch_salt_lantern',
+        'silly_oddities:jungle_salt_lantern',
+        'silly_oddities:acacia_salt_lantern',
+        'silly_oddities:dark_oak_salt_lantern',
+        'silly_oddities:mangrove_salt_lantern',
+        'silly_oddities:cherry_salt_lantern',
+        'silly_oddities:bamboo_salt_lantern',
+        'silly_oddities:crimson_salt_lantern',
+        'silly_oddities:warped_salt_lantern'*/ // They dont display the flame sadly :(
+    ])
+})
