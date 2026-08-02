9b3c80f77624d6017cc6ea2dcec6054e1b2331af
pafeuu <pawel.mejerr@gmail.com>
2026-08-02 19:54:43 +0200

    removed coprsecomplex recipe

--- kubejs/server_scripts/machines/apparatus.js
+++ kubejs/server_scripts/machines/apparatus.js
@@ -2,13 +2,14 @@ ServerEvents.recipes(event=>{
 
     const apparatus = event.recipes.ars_nouveau.enchanting_apparatus
 
-    apparatus([
+    /*apparatus([
         "bone_block",
         "kubejs:rotten_flesh_block",
         "quark:soul_bead"],
         "kubejs:warp_scroll",
         "corpsecomplex:scroll",
         1000).id("corpsecomplex:scroll")
+    */
 
     apparatus([
         "#forge:gears/arcane_gold",
